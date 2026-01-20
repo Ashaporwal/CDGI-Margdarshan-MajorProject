61559b6355d8951e6a3089e8ffc691e4fe3baefc
ashap127 <porwalasha58@gmail.com>
2026-01-20 15:37:29 +0530

    middleware

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../model/user.model.js";
+
+export const protect = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "Not authorized" });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        
+        const decoded = jwt.verify(token, process.env.JWT || "secretkey");
+
+        
+        const user = await User.findById(decoded.id).select("-password");
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user; 
+        next();
+
+    } catch (err) {
+        console.log("Auth error:", err);
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
