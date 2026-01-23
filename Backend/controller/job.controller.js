c1afdc4b7cf1af27ad0e3a41caad00bbd46af44e
Himashi <himanshijoshi11d8655@gmail.com>
2026-01-23 22:12:59 +0530

    Job Section

---
+++ Backend/controller/job.controller.js
@@ -0,0 +1,77 @@
+import { Job } from "../model/job.model.js";
+
+export const createJob = async (req, res) => {
+    try {
+        const job = await Job.create({
+            ...req.body,
+            postedBy: req.user.id
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "Job created successfully",
+            job
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+
+export const getAllJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find().populate("postedBy", "name email role").sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            count: jobs.length,
+            jobs
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+
+export const getJobById = async (req, res) => {
+    try {
+        const job = await Job.findById(req.params.id).populate("postedBy", "name email role");
+        if (!job) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.status(200).json({ success: true, job });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+
+export const updateJob = async (req, res) => {
+    try {
+        const job = await Job.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Job updated successfully",
+            job
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+
+export const deleteJob = async (req, res) => {
+    try {
+        await Job.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Job deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
---
+++ Backend/router/job.routes.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { body, param } from "express-validator";
+import { createJob, getAllJobs, getJobById, updateJob, deleteJob } from "../controller/job.controller.js";
+import {validate} from "../middleware/validate.js";
+import { protect } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+
+router.post( "/job",
+  protect ,
+  body("title", "Title is required").notEmpty(),
+  body("company", "Company is required").notEmpty(),
+  body("jobType", "Job type must be Full-time or Internship")
+    .isIn(["Full-time", "Internship"]),
+  body("location", "Location is required").notEmpty(),
+  body("description", "Description is required").notEmpty(),
+  body("deadline", "Deadline must be a valid date").isISO8601(),
+  validate,
+  createJob
+);
+
+router.get("/jobs", protect ,getAllJobs);
+
+router.get(
+  "jobs/:id",
+  protect ,
+  param("id", "Invalid Job ID").isMongoId(),
+  validate,
+  getJobById
+);
+
+router.put(
+  "/jobs/:id",
+  protect ,
+  param("id", "Invalid Job ID").isMongoId(),
+  body("jobType")
+    .optional()
+    .isIn(["Full-time", "Internship"])
+    .withMessage("Job type must be Full-time or Internship"),
+  body("deadline")
+    .optional()
+    .isISO8601()
+    .withMessage("Deadline must be a valid date"),
+
+  validate,
+  updateJob
+);
+
+router.delete(
+  "/jobs/:id",
+  protect ,
+  param("id", "Invalid Job ID").isMongoId(),
+  validate,
+  deleteJob
+);
+
+export default router;
