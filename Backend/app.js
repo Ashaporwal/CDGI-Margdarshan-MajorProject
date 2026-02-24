7738d0a2c0ffae9f22723f53421deaaf119157b2
Himashi <himanshijoshi11d8655@gmail.com>
2026-02-24 11:27:56 +0530

    Add 404 Error handler

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import bodyParser from "body-parser";
+import jobRouter from "./router/job.routes.js";
+import userRouter from "./router/user.routes.js";
+import adminRouter from "./router/admin.routes.js"
+import alumniRouter from "./router/alumni.routes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
- 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-mongoose.connect("mongodb://localhost:27017/cdgiMargdarshan").then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log("MongoDB connection error:", err));
- 
+app.use("/app" , userRouter);
+app.use("/app" , jobRouter);
+app.use("/admin" , adminRouter);
+app.use("/" , alumniRouter);
 
-    
-app.listen(3000, () => console.log("Server started"));
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
+});
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+    app.listen(process.env.PORT, () => {
+      console.log("Server started on port : " , process.env.PORT);
+    });
+  })
+  .catch(error => {
+    console.log("Database connection failed:", error);
+  });
