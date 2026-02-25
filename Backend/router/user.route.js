f8a0e900b9e9491313f209de40f27169700c8f6c
ashap127 <porwalasha58@gmail.com>
2026-02-25 21:58:41 +0530

    router fix

---
+++ Backend/router/user.route.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  changePassword,
+  get,
+  login,
+  logout,
+  register,
+  updateProfile,
+} from "../controller/user.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/get", protect, get);
+
+
+router.put(
+  "/profile",
+  protect,
+  upload.single("photo"), // IMPORTANT
+  updateProfile
+);
+
+router.put("/changePassword", protect, changePassword);
+router.post("/logout", protect, logout);
+
+export default router;
