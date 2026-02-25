3b9fba6b17558948c109fa01383918a6e6996155
Himashi <himanshijoshi11d8655@gmail.com>
2026-02-25 09:48:44 +0530

    Updated

--- Backend/router/job.routes.js
+++ Backend/router/job.routes.js
@@ -1,18 +1,22 @@
 import express from "express";
 import { body, param } from "express-validator";
-import { createJob, getAllJobs, getJobById, updateJob, deleteJob } from "../controller/job.controller.js";
-import {validate} from "../middleware/validate.js";
+import {
+  createJob,
+  getAllJobs,
+  getJobById,
+  updateJob,
+  deleteJob
+} from "../controller/job.controller.js";
+
+import { validate } from "../middleware/validate.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
-
-router.post( "/job",
-=======
-router.post("/job",
->>>>>>> origin/main
-  protect ,
+// CREATE JOB
+router.post(
+  "/job",
+  protect,
   body("title", "Title is required").notEmpty(),
   body("company", "Company is required").notEmpty(),
   body("jobType", "Job type must be Full-time or Internship")
@@ -24,23 +28,22 @@ router.post("/job",
   createJob
 );
 
-router.get("/jobs", protect ,getAllJobs);
+// GET ALL
+router.get("/jobs", protect, getAllJobs);
 
+// GET BY ID
 router.get(
-<<<<<<< HEAD
-  "jobs/:id",
-=======
   "/jobs/:id",
->>>>>>> origin/main
-  protect ,
+  protect,
   param("id", "Invalid Job ID").isMongoId(),
   validate,
   getJobById
 );
 
+// UPDATE
 router.put(
   "/jobs/:id",
-  protect ,
+  protect,
   param("id", "Invalid Job ID").isMongoId(),
   body("jobType")
     .optional()
@@ -50,14 +53,14 @@ router.put(
     .optional()
     .isISO8601()
     .withMessage("Deadline must be a valid date"),
-
   validate,
   updateJob
 );
 
+// DELETE
 router.delete(
   "/jobs/:id",
-  protect ,
+  protect,
   param("id", "Invalid Job ID").isMongoId(),
   validate,
   deleteJob
