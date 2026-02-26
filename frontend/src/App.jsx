8fbc9e3fe66721b054c6c5af551a696edcaef237
ashap127 <porwalasha58@gmail.com>
2026-02-26 11:35:45 +0530

    /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
 {/* 
         <Route path="/profile" element={
