061f52a703f53f11923c513954b5d207a94546d6
ashap127 <porwalasha58@gmail.com>
2026-02-26 10:36:41 +0530

    sign up fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/profile"
-          element={
+{/* 
+        <Route path="/profile" element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
-        />
-
+        /> */}
+<Route path="/editprofile" element={<ProfilePage/>} />
         <Route
           path="/dashboard"
           element={
