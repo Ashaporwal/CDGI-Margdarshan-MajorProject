cd5b84637e4c4500c602542f0b351f7ba2e5964b
Himashi <himanshijoshi11d8655@gmail.com>
2026-02-28 07:41:16 +0530

    Protected Route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,28 @@
+// import { Navigate } from "react-router-dom";
+
+// function ProtectedRoute({ children }) {
+//   const token = localStorage.getItem("token");
+
+//   if (!token) {
+//     return <Navigate to="/login" />;
+//   }
+
+//   return children;
+// }
+
+// export default ProtectedRoute;
+
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children, role }) {
   const token = localStorage.getItem("token");
+  const storedUser = JSON.parse(localStorage.getItem("user"));
 
   if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  if (role && storedUser?.role !== role) {
     return <Navigate to="/login" />;
   }
 
