434198f7648f29cde5a1ade2fb48825f246678e4
ashap127 <porwalasha58@gmail.com>
2026-03-01 16:55:04 +0530

    all changes done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/user",
+  // baseURL: "http://localhost:5000",
+   baseURL: "http://localhost:5000/user", 
 });
 
 API.interceptors.request.use((req) => {
