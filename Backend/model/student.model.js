977565a2ee56db98d618469cc4c802e35ff284ca
ashap127 <porwalasha58@gmail.com>
2026-03-01 12:25:19 +0530

    fields added

--- Backend/model/student.model.js
+++ Backend/model/student.model.js
@@ -1,44 +1,66 @@
+
+
 import mongoose from "mongoose";
 
 const studentProfileSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    enrollmentNumber: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    course: {
-        type: String, 
-        required: true,
-        trim: true
-    },
-    yearOfStudy: {
-        type: Number,
-        required: true,
-        min: 1, 
-        max: 6
-    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    enrollmentNumber: String,
+    course: String,
+    yearOfStudy: Number,
+    cgpa: Number,
+    address: String,
+    skills: String,
+    linkedin: String,
+    github: String,
+    portfolio: String,
+    resume: String,
+    profilePic: String
+});
 
-    skills: {
-        type: [String],     
-        default: []
-    },
-    resumeUrl: {
-        type: String,
-        trim: true
-    },
-    placementStatus: {
-        type: String,
-        enum: ["Not Placed", "Placed"],
-        default: "Not Placed"
-    }
-}, { timestamps: true });
-const StudentProfile = mongoose.model("StudentProfile", studentProfileSchema);
+export default mongoose.model("StudentProfile", studentProfileSchema);
 
-export default StudentProfile;
+
+// import mongoose from "mongoose";
+
+// const studentProfileSchema = new mongoose.Schema({
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//         required: true,
+//         unique: true
+//     },
+//     enrollmentNumber: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         trim: true
+//     },
+//     course: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     yearOfStudy: {
+//         type: Number,
+//         required: true,
+//         min: 1,
+//         max: 6
+//     },
+
+//     skills: {
+//         type: [String],
+//         default: []
+//     },
+//     resumeUrl: {
+//         type: String,
+//         trim: true
+//     },
+//     placementStatus: {
+//         type: String,
+//         enum: ["Not Placed", "Placed"],
+//         default: "Not Placed"
+//     }
+// }, { timestamps: true });
+// const StudentProfile = mongoose.model("StudentProfile", studentProfileSchema);
+
+// export default StudentProfile;
