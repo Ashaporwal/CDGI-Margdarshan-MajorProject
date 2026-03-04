1e6609a91e374a8cf57fab7c6997149bda6a2274
ashap127 <porwalasha58@gmail.com>
2026-03-04 11:27:37 +0530

    alumni-dashboard

---
+++ frontend/src/pages/alumni/AlumniDashboard.jsx
@@ -0,0 +1,21 @@
+// import AlumniLayout from "./AlumniLayout";
+
+function AlumniDashboard() {
+    // if(user?.role !== "alumni"){
+    //     return null;
+    // }
+  return (
+    <>
+    
+      <h1 className="text-2xl font-bold mb-2">
+        Welcome to Alumni Dashboard
+      </h1>
+      <p className="text-gray-500">
+        Manage jobs, mentorship and your professional profile.
+      </p>
+    
+    </>
+  );
+}
+
+export default AlumniDashboard;
