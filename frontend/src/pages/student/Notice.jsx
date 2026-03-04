be66cb710be578d18f86da8da81f3ab652102096
ashap127 <porwalasha58@gmail.com>
2026-03-04 11:23:37 +0530

    notice-page

---
+++ frontend/src/pages/student/Notice.jsx
@@ -0,0 +1,10 @@
+function Notice() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">College Notices</h1>
+      <p className="text-gray-600">All important announcements will appear here.</p>
+    </div>
+  );
+}
+
+export default Notice;
