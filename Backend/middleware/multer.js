ba540b9062b89b2fdbc851fd4c07ec0c04fa7c1d
Himashi <himanshijoshi11d8655@gmail.com>
2026-03-05 23:03:09 +0530

    Done for Both resume/photos

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,13 +1,41 @@
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// export const upload = multer({ storage });
 import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
+
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+
+    if (file.fieldname === "photo") {
+      cb(null, "uploads/photos");
+    } 
+    
+    else if (file.fieldname === "resume") {
+      cb(null, "uploads/resumes");
+    }
+
+    else {
+      cb(null, "uploads");
+    }
+
   },
+
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
-  },
+  }
+
 });
 
 export const upload = multer({ storage });
