6e2ef02e0456f87d3a8a1e01852f5b70be46bc06
ashap127 <porwalasha58@gmail.com>
2026-03-06 08:47:35 +0530

    update feature added

--- Backend/controller/alumni.controller.js
+++ Backend/controller/alumni.controller.js
@@ -76,7 +76,67 @@ export const updateAlumniProfile = async (req, res) => {
 };
 
 
+export const updatedAlumniStatus = async(req,res)=>{
+  try{
+
+    if(req.user.role !== "admin"){
+      return res.status(403).json({message:"only admin allowed"});
+    } 
+    const {status} = req.body;
+
+    const alumni = await AlumniProfile.findByIdAndUpdate(
+      req.params.id,
+      {status},
+      {new:true}
+
+    );
+    if(!alumni){
+      return res.status(404).json({message:"alumni not found"});
+    }
+    res.status(200).json(alumni);
+   }catch(error){
+    console.log(error);
+    return res.status(500).json({message:"internal server error",error});
+  }
+}
+
+export const getAlumniById = async (req, res) => {
+  try {
+
+    const profile = await AlumniProfile.findById(req.params.id)
+      .populate("userId", "name email department graduationYear");
+
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    res.status(200).json(profile);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 /* ADMIN: GET ALL */
+// export const getAllAlumniProfiles = async (req, res) => {
+//   try {
+
+//     if (req.user.role !== "admin") {
+//       return res.status(403).json({ message: "Only admin allowed" });
+//     }
+
+//     const profiles = await AlumniProfile.find()
+//       .populate("userId", "name email department graduationYear");
+
+//     res.status(200).json(profiles);
+
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
+
 export const getAllAlumniProfiles = async (req, res) => {
   try {
 
@@ -84,13 +144,51 @@ export const getAllAlumniProfiles = async (req, res) => {
       return res.status(403).json({ message: "Only admin allowed" });
     }
 
-    const profiles = await AlumniProfile.find()
-      .populate("userId", "name email department graduationYear");
+    const page = Number(req.query.page) || 1;
+    const limit = 10;
 
-    res.status(200).json(profiles);
+    const skip = (page - 1) * limit;
+
+    const profiles = await AlumniProfile.find()
+      .populate("userId", "name email department graduationYear")
+      .skip(skip)
+      .limit(limit);
+
+    const total = await AlumniProfile.countDocuments();
+
+    res.status(200).json({
+      total,
+      page,
+      pages: Math.ceil(total / limit),
+      profiles
+    });
 
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+// export const getAllAlumniProfiles = async (req, res) => {
+//   try {
+
+//     console.log("USER:", req.user);
+//     console.log("ROLE:", req.user.role);
+
+//     if (req.user.role !== "admin") {
+//       return res.status(403).json({ message: "Only admin allowed" });
+//     }
+
+//     const profiles = await AlumniProfile.find()
+//       .populate("userId", "name email department graduationYear");
+
+//     console.log("PROFILES:", profiles);  // 👈 add this
+
+//     res.status(200).json(profiles);
+
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
