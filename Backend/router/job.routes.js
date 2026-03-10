3ba99ce272bda11942fdebe5e37f278273c31b13
Himashi <himanshijoshi11d8655@gmail.com>
2026-03-10 09:33:26 +0530

    Job router

--- Backend/router/job.routes.js
+++ Backend/router/job.routes.js
@@ -21,10 +21,10 @@ router.post(
   body("jobType", "Job type must be Full-time or Internship")
     .isIn(["Full-time", "Internship"]),
   body("location", "Location is required").notEmpty(),
-  body("description", "Description is required").notEmpty(),
+  // body("description", "Description is required").notEmpty(),
   // body("deadline", "Deadline must be a valid date").isISO8601(),
-body("minCGPA").optional({ checkFalsy: true }).isFloat({ min: 0, max: 10 }),
-body("requirements").optional().isArray(),
+// body("minCGPA").optional({ checkFalsy: true }).isFloat({ min: 0, max: 10 }),
+// body("requirements").optional().isArray(),
 body("skills").optional().isArray(),
   validate,
   createJob
