0c0df28d72a5d2b7be04aeb45ad09d98f4a07e81
Himashi <himanshijoshi11d8655@gmail.com>
2026-03-10 09:36:56 +0530

    Update for role based access

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,29 +1,15 @@
-// import { Navigate } from "react-router-dom";
-
-// function ProtectedRoute({ children }) {
-//   const token = localStorage.getItem("token");
-
-//   if (!token) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   return children;
-// }
-
-// export default ProtectedRoute;
-
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, role }) {
   const token = localStorage.getItem("token");
-  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedUser = JSON.parse(localStorage.getItem("user") || "{}");
 
   if (!token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
   if (role && storedUser?.role !== role) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
